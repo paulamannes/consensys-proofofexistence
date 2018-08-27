46317a2fed1c60d188cdcf13e0daab51024263ff
Paula Mannes <me@paula.land>
2018-08-27 04:10:41 -0400

    Add table to visualize all previous files from account 🍡

--- src/layouts/home/IpfsRetrieveContractForm.js
+++ src/layouts/home/IpfsRetrieveContractForm.js
@@ -6,38 +6,71 @@ class IpfsRetrieveContractForm extends Component {
   constructor(props, context) {
     super(props);
 
-    this.callContractMethod = this.callContractMethod.bind(this)
-
     this.contracts = context.drizzle.contracts;
 
     this.state = {
-      message:'',
-      file:'',     
-      buffer:'',
-      ipfsHash:'',
-      transactionHash:'',
-      txReceipt: '',
-      block:'',
-      blockTimestamp:''
+        files: []
     }
   }
 
-  callContractMethod(){
-    console.log('hey, now I wanna retrieve the into on the blockchain!');
-
-    this.contracts["IpfsStorage"].methods.getFileIndexes().send({from: this.account },
+  componentWillMount(){
+    this.contracts["IpfsStorage"].methods.getFileIndexes().call({from: this.account },
       (error, indexArray) => {
-      }); 
+          indexArray.map((item) => {
+            return(
+              this.contracts["IpfsStorage"].methods.getFile(item).call({from: this.account },
+               (error, file) => {                   
+                    console.log(file);
+                    var files = this.state.files.concat({
+                        index: item,
+                        fileName:file[0],
+                        ipfsHash:file[1],
+                        tags:file[2],
+                        timestamp:file[3]
+                    });
+                    
+                    this.setState({ files: files });
+              })
+            )
+          })
+      });
   }
 
   render() {
     return (
       <div>
-        <div className="row mt">  
-          <div className="col-lg-12">  
-          hellow
+        <div className="row">
+          <div className="col-md-12">
+            <div className="content-panel">
+              <h4><i className="fa fa-angle-right"></i> All files from user</h4>
+              <hr/>
+              <table className="table">
+                <thead>
+                  <tr>
+                    <th>#</th>
+                    <th>File name</th>
+                    <th>Tags</th>
+                    <th>IPFS Link</th>
+                    <th>Timestamp</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {this.state.files.map((item) => {
+                     return(
+                      <tr key={item.index}>
+                        <td>{item.index}</td>
+                        <td>{item.fileName}</td>
+                        <td>{item.tags}</td>
+                        <td><a href={`https://gateway.ipfs.io/ipfs/${item.ipfsHash}`} target="_blanck">{ item.ipfsHash }</a></td>
+                        <td>{item.timestamp}</td>
+                      </tr>
+                      )
+                  })}
+                </tbody>
+              </table>
+            </div>
           </div>
-        </div>
+      </div>
       </div>
     )
   }
